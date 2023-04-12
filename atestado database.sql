a6cddf995d964e86e089f65091c9c6b1dd84bdb2
Lays Maiara <lays.devstudio@gmail.com>
2023-04-12 20:55:55 -0300

    Banco de dados postgreSQL

---
+++ atestado database.sql
@@ -0,0 +1,13 @@
+-- Database: auto_atestado
+
+-- DROP DATABASE IF EXISTS auto_atestado;
+
+CREATE DATABASE auto_atestado
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
